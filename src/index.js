aeef2d75e29fa818261c95aa836afca6d0b91449
Willian S <wsapcf@gmail.com>
2019-09-24 11:49:57 -0300

    Template com Reactotron

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text, StatusBar } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
